4679fce14e1fa8e815b24a097d6cb35ed4002e40
OscardeKeijzer <71188715+OscardeKeijzer@users.noreply.github.com>
2020-11-27 23:14:42 +0100

    Added calculations of waiting times and sojourn times

--- OscardeKeijzer_12516112_KamleshSahoo12821721 Assignment 2_.py
+++ OscardeKeijzer_12516112_KamleshSahoo12821721 Assignment 2_.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 # Initialize simulation parameters
-n = 5    # Total number of customers
+n = 50    # Total number of customers
 c = 0    # Customer id counter
 lamda = 5    # Arrival rate (lambda spelling is taken)
 mu = 10    # Service rate
@@ -37,17 +37,19 @@ class Customer(object):
 
     def customer(self, env, id, serviceCenter, arrival_time, service_duration):    # Customer simulation function
         # Simulate arrival
-        yield env.timeout(arrival_time)
+        yield env.timeout(arrival_time)    # Yield until time of arrival has passed
         
         # Request service
         print('Arrival of %s at t = %s' % (id, env.now))
-        with serviceCenter.request() as req:
-            yield req
+        with serviceCenter.request() as req:    # Request service
+            yield req    # Yield until service available
             
             # Receive service
             print('%s started receiving service at t = %s' % (id, env.now))
+            self.q_time = env.now - self.a_time    # Calculate waiting time
             yield env.timeout(service_duration)
             print('%s left the service center at t = %s' % (id, env.now))
+            self.sj_time = self.q_time + self.s_time    # Calculate sojourn time
 
 # Initialize SimPy simulation
 env = simpy.Environment()    # Initiate the SimPy environment
@@ -56,26 +58,46 @@ serviceCenter = simpy.Resource(env, capacity=1)    # Initiate server resource, c
 for i in range(n):    # Generate and simulate n customers
     c = c + 1    # Create customer id c (NOT directly related to arrival time)
     i = Customer(c, env)    # Create ith customer object with id c
-    env.process(i.customer(env,'Customer id %d' % i.id, serviceCenter, i.a_time, i.s_time))
-    list.append(i)
+    env.process(i.customer(env,'Customer id %d' % i.id, serviceCenter, i.a_time, i.s_time))    # Simulate ith customer
+    list.append(i)    # Add ith customer to list of customers
 
 env.run()    # Run SimPy simulation
 
 # Print simulation results
 print('\n')
 
+'''
 for i in list:
-    print(('Customer id %d arrival time was t = %s and service time was %s') % (i.id, i.a_time, i.s_time))
+    print(('Customer id %d arrival time was t = %s, service time was %s, and waiting time was %s') % (i.id, i.a_time, i.s_time, i.q_time))
+'''
     
 print('\nMean customer arrival time was %s ' % ((sum(i.a_time for i in list)/n)))
+print('\nMean customer waiting time was %s ' % ((sum(i.q_time for i in list)/n)))
+print('\nMean customer service time was %s ' % ((sum(i.s_time for i in list)/n)))
+print('\nMean customer sojourn time was %s ' % ((sum(i.sj_time for i in list)/n)))
 
-'''
+
+#'''
+# Plot results
+fig, ax = plt.subplots()
+id_list = []
+a_list = []
+for i in list:
+    a_list.append(i.a_time)
+    id_list.append(i.id)
+ax.scatter(id_list,a_list,color='black')
+ax.set(xlabel = 'Customer ID')
+ax.set(ylabel = 'Arrival Time')
+#'''
+
+#'''
 # Plot a(t) and A(t) functions
+fig2, ax2 = plt.subplots()
 t = np.linspace(0,1)
-plt.scatter(t,a(t), label='$\mathit{a(t)}$')
-plt.scatter(t,A(t), label='$\mathit{A(t)}$')
-plt.scatter(t,b(t), label='$\mathit{b(t)}$')
-plt.scatter(t,B(t), label='$\mathit{B(t)}$')
-plt.legend()
-plt.xlabel('$\mathit{t}$')
-'''
+ax2.scatter(t,a(t), label='$\mathit{a(t)}$')
+ax2.scatter(t,A(t), label='$\mathit{A(t)}$')
+ax2.scatter(t,b(t), label='$\mathit{b(t)}$')
+ax2.scatter(t,B(t), label='$\mathit{B(t)}$')
+ax2.set(xlabel = 'Simulation Time $\mathit{t}$')
+ax2.legend()
+#'''
